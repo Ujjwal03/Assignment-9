ad2a7d070146085abcaeb7857a710abe67ed8928
Ujjwal Garg <113996919+Ujjwal03@users.noreply.github.com>
2023-01-19 09:04:57 +0530

    Create value.cpp

---
+++ value.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  int find(vector<int>& nums,int original)
+  {
+    unordered_set<int>check;
+    for(auto &&i : nums)
+    {
+      check.insert(i);
+    }
+    while(check.find(original)!=check.end())
+    {
+       original=original*2;
+    }
+    return original;
+  }  
+}  
+    
